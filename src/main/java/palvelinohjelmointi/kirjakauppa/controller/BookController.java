ddd3ce0d6c8a9eff534fa5b30b3a10a266045794
tommimaki <bgp010@myy.haaga-helia.fi>
2021-09-20 20:48:16 +0300

    bookviewlist added

--- src/main/java/palvelinohjelmointi/kirjakauppa/controller/BookController.java
+++ src/main/java/palvelinohjelmointi/kirjakauppa/controller/BookController.java
@@ -1,17 +1,36 @@
 package palvelinohjelmointi.kirjakauppa.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import palvelinohjelmointi.kirjakauppa.domain.BookRepository;
+
 
 @Controller
 public class BookController {
+	
+	// indexsivu Getillä
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String bookTitle(@RequestParam(value="title") String title, Model model) {
 		model.addAttribute("title", title);
 		return "kirjakauppa";
 }
+	@Autowired
+	private BookRepository repository;
+	
+	//listview luonti
+	@RequestMapping(value = "/booklist", method = RequestMethod.GET)
+	public String Booklist(Model model) {
+		//key attribuutille "listan nimi", toka parametri = value, kirjat jotka saadaan tietokannasta findAll avulla.
+		model.addAttribute("books", repository.findAll());
+		//return stringi, templaten nimi tähän, joka luodaan seuraavaksi
+		return "booklist";
+	}
+	
+	
+	
 }
