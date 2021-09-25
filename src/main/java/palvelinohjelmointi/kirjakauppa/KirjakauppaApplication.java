84dc3c73e1f62f51dfa59afca8aea7cae6c05f90
tommimaki <bgp010@myy.haaga-helia.fi>
2021-09-26 01:11:01 +0300

    4.3 one to many changes added

--- src/main/java/palvelinohjelmointi/kirjakauppa/KirjakauppaApplication.java
+++ src/main/java/palvelinohjelmointi/kirjakauppa/KirjakauppaApplication.java
@@ -24,22 +24,22 @@ public class KirjakauppaApplication {
 	@Bean
 	public CommandLineRunner demo(BookRepository repository, CategoryRepository catrep ) {
 	return (args) -> {
-	 Book Harry1 = new Book("Härri Pötter ja Äzkäbän", "J.K. Rowling" , 2001, "12314", 20);
-	 Book Harry2 = new Book("Härri Pötter ja Tunkkainen Pikari", "J.K. Rowling" , 2004, "12322", 23);
-	 Book Harry3 = new Book("Härri Pötter ja Kuoleman Ahmatit", "J.K. Rowling" , 2006, "123432", 30);
-	 
-	 Category Fiction = new Category("Fiction");
-	 Category Fantasy = new Category("Fantasy");
-	 Category Horror = new Category("Horror");
+		
+		 Category Fiction = new Category("Fiction");
+		 Category Fantasy = new Category("Fantasy");
+		 Category Horror = new Category("Horror");
+		  
+		 catrep.save(Fiction);
+		 catrep.save(Fantasy);
+		 catrep.save(Horror);
+		 
+		 repository.save(new Book("Härri Pötter ja Äzkäbän", "J.K. Rowling" , 2001, "12314", 20, Fiction ));
+		 repository.save(new Book("Härri Pötter ja viisasten kivi", "J.K. Rowling" , 2001, "12314", 20, Fantasy ));
+		 repository.save(new Book("Härri Pötter ja kuolemien varjelukset", "J.K. Rowling" , 2001, "12314", 20, Horror ));
+	
+		
 	  
-	 
-	 repository.save(Harry1);
-	 repository.save(Harry2);
-	 repository.save(Harry3);
-	 
-	 catrep.save(Fiction);
-	 catrep.save(Fantasy);
-	 catrep.save(Horror);
+
 		};
 	}
 
--- src/main/java/palvelinohjelmointi/kirjakauppa/domain/Book.java
+++ src/main/java/palvelinohjelmointi/kirjakauppa/domain/Book.java
@@ -36,13 +36,14 @@ public class Book {
 
 	public Book	() {}
 	
-	public Book(String title, String author, int year, String isbn, double price) {
+	public Book(String title, String author, int year, String isbn, double price, Category category) {
 		super();
 		this.title = title;
 		this.author = author;
 		this.year = year;
 		this.isbn = isbn;
 		this.price = price;
+		this.category = category;
 		
 		
 	}
@@ -98,6 +99,10 @@ public class Book {
 	
 	@Override
 	public String toString() {
-		return "Book: " + title + author + year + isbn + price ;
+		if(this.category != null)
+		return "Book: " + title + author + year + isbn + price + "Category= " + this.getCategory();
+		else 
+			return "Book: " + title + author + year + isbn + price;
 }
+	
 }
